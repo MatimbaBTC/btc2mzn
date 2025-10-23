a244fdde6185a4b951d0335a7433bc98d486f826
CUSTcoding <custodiotitossetitosse@gmail.com>
2025-10-23 18:49:30 -0500

    feat: add USD to btc converter service

--- mzn_convert_btc/core/services.py
+++ mzn_convert_btc/core/services.py
@@ -3,13 +3,13 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-# Carregar variáveis de ambiente
+
 BASE_DIR = Path(__file__).resolve().parent
 ROOT_DIR = BASE_DIR.parent.parent
 load_dotenv(ROOT_DIR / '.env')
 
 EXCHANGE_API = os.getenv('EXCHANGE_API')
-
+COINGECKO_API = os.getenv('COINGECKO_API')
 
 class ExchangeService:
 
@@ -32,20 +32,39 @@ class ExchangeService:
     @staticmethod
     def convert_mzn_to_usd(mzn_amount):
 
-        rate = ExchangeService.get_usd_per_mzn()
-
-        if rate is None:
+        try:
+            resp = requests.get(EXCHANGE_API)
+            resp.raise_for_status()
+            data = resp.json()
+            usd_rate = data['rates']['USD']  # já é MZN → USD
+            return round(mzn_amount * usd_rate, 2)
+        except Exception as e:
+            print("Erro ao buscar taxa de câmbio:", e)
             return None
-        return round(mzn_amount * rate, 2)
-    
-
+        
+    @staticmethod
+    def convert_usd_to_btc(usd_amount):
+        try:
+            url = f'{COINGECKO_API}'
+            params = {"ids": "bitcoin", "vs_currencies": "usd"}
+            resp = requests.get(url, params=params)
+            resp.raise_for_status()
+            data = resp.json()
+            btc_price_usd = data['bitcoin']['usd']
+            btc_amount = usd_amount / btc_price_usd
+            return round(btc_amount, 8)
+        except Exception as e:
+            print("Erro ao buscar preço do Bitcoin:", e)
+            return None
+        
 
 if __name__ == "__main__":
     try:
         valor_mzn = float(input("Digite o valor em MZN: "))
         usd = ExchangeService.convert_mzn_to_usd(valor_mzn)
+        btc = ExchangeService.convert_usd_to_btc(usd)
         if usd is not None:
-            print(f"{valor_mzn} MZN = {usd} USD")
+            print(f"{valor_mzn} MZN = {usd} USD = {btc} BTC")
         else:
             print("Não foi possível calcular a conversão.")
     except ValueError:
